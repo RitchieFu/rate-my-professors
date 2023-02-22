913259a24d46c3f55180531f6da267a440524edd
Sazhelle GM <57726772+ChromeSkullex@users.noreply.github.com>
2023-02-21 19:02:02 -0500

    Added  Access-Control-Allow-Origin

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {AUTH_TOKEN} from './constants';
 
 const client = new GraphQLClient('https://www.ratemyprofessors.com/graphql', {
   headers: {
-    authorization: `Basic ${AUTH_TOKEN}`
+    authorization: `Basic ${AUTH_TOKEN}`, 'Access-Control-Allow-Origin': '*'
   }
 });
 
